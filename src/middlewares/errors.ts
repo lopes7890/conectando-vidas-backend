d96746155807ab278dcf977e28b79dd925541f31
lopes7890 <lopesallison414@gmail.com>
2025-04-17 23:22:43 -0300

    modify middlewares errors and adding prisma to build

--- src/middlewares/errors.ts
+++ src/middlewares/errors.ts
@@ -1,18 +1,7 @@
-import { NextFunction, Request, Response } from "express"
-import { ApiError } from "../helpers/api-errors.js"
-import { ErrorRequestHandler } from "express";
+import { Request, Response, NextFunction } from 'express'
 
-export const errorMiddleware: ErrorRequestHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  if (err instanceof ApiError) {
-    return res.status(err.statusCode).json({ message: err.message });
-  }
-
-  console.error("internal error", err);
-
-  return res.status(500).json({ message: "Internal Server Error" });
+export function errorMiddleware(err: any, req: Request, res: Response, next: NextFunction): void {
+  console.error(err)
+  res.status(500).json({ error: "internal fail, try again" })
 }
+
