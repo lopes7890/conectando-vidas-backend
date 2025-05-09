9444bd005079e109072ed3de732f4194d12b5ab5
lopes7890 <lopesallison414@gmail.com>
2025-05-09 12:52:57 -0300

    correction in verifcation token on register animal routes

--- build/controllers/animals/newAnimalController.js
+++ build/controllers/animals/newAnimalController.js
@@ -1,20 +1,15 @@
 // import class
 import { NewAnimalService } from "../../services/animals/registerAnimals.js";
-import { UnauthorizedError } from "../../helpers/api-errors.js";
 class NewAnimalController {
     async newAnimal(req) {
         try {
-            const typeUser = req.user;
-            //if(typeUser.tipo === "admin" || typeUser.tipo === "doador"){
             const newService = new NewAnimalService();
             const registerNewAnimal = await newService.registerAnimalInDataBase(req.body, req);
             if (typeof registerNewAnimal === "object") {
                 return "internal fail, try again";
-                //};
-                return registerNewAnimal;
             }
             ;
-            throw new UnauthorizedError("not authorized");
+            return registerNewAnimal;
         }
         catch (error) {
             return { message: error };
--- src/controllers/animals/newAnimalController.ts
+++ src/controllers/animals/newAnimalController.ts
@@ -2,24 +2,18 @@ import { Request } from "express";
 
 // import class
 import { NewAnimalService } from "../../services/animals/registerAnimals.js";
-import { UnauthorizedError } from "../../helpers/api-errors.js";
-
 
 class NewAnimalController {
     async newAnimal(req: Request) {
         try{
-            const typeUser = req.user;
-            //if(typeUser.tipo === "admin" || typeUser.tipo === "doador"){
-                const newService = new NewAnimalService();
-                const registerNewAnimal = await newService.registerAnimalInDataBase(req.body, req);
-                if(typeof registerNewAnimal === "object"){
-                    return "internal fail, try again";
-                //};
-    
-                return registerNewAnimal;
-            };
+            const newService = new NewAnimalService();
+            const registerNewAnimal = await newService.registerAnimalInDataBase(req.body, req);
 
-            throw new UnauthorizedError("not authorized");
+            if (typeof registerNewAnimal === "object"){
+                return "internal fail, try again"
+            };
+    
+            return registerNewAnimal;
 
         } catch (error) {
             return {message: error};
