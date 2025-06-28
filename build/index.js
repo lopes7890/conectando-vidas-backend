f8227b3f106265cd4a8d157a9cde6a525475df7b
lopes7890 <lopesallison414@gmail.com>
2025-06-28 12:14:48 -0300

    configuration CORS

--- build/index.js
+++ build/index.js
@@ -5,16 +5,20 @@ import cors from "cors";
 import "dotenv/config";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import path from "path";
 // routes
 import userRoutes from "./routes/userRoutes.js";
 import animalsRoutes from "./routes/animalsRoutes.js";
 import ongsRouter from "./routes/ongsRoutes.js";
 import storiesRoutes from "./routes/successStoriesRoutes.js";
 import voluntaryRoutes from "./routes/voluntaryRoutes.js";
-import path from "path";
 const app = express();
 const port = process.env.PORT;
-app.use(cors());
+app.use(cors({
+    origin: 'https://conectando-vidas-frontend.vercel.app',
+    methods: 'GET, POST, PUT, DELETE',
+    credentials: true
+}));
 app.use(express.json());
 app.use(userRoutes);
 app.use(animalsRoutes);
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cors from "cors";
 import "dotenv/config";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import path from "path";
 
 // routes
 import userRoutes from "./routes/userRoutes.js";
@@ -12,12 +13,17 @@ import animalsRoutes from "./routes/animalsRoutes.js";
 import ongsRouter from "./routes/ongsRoutes.js";
 import storiesRoutes from "./routes/successStoriesRoutes.js";
 import voluntaryRoutes from "./routes/voluntaryRoutes.js";
-import path from "path";
+
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://conectando-vidas-frontend.vercel.app',
+    methods: 'GET, POST, PUT, DELETE',
+    credentials: true
+}));
+
 app.use(express.json());
 
 app.use(userRoutes);
